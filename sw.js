30b84e611102167c7226593ed048bd96459a09ea
kanza-12 <sp25-elc-034@cuilahore.edu.pk>
2026-04-25 21:27:47 +0500

    sw.js

--- sw.js
+++ sw.js
@@ -1,44 +1,38 @@
-const CACHE_NAME = "kanza-cache-v1";
-
-const FILES_TO_CACHE = [
+const CACHE_NAME = "kanza-ultimate-v1";
+const ASSETS_TO_CACHE = [
   "./",
   "./index.html",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
+  "https://unpkg.com/react@18/umd/react.production.min.js",
+  "https://unpkg.com/react-dom@18/umd/react-dom.production.min.js",
+  "https://unpkg.com/@babel/standalone/babel.min.js",
+  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
 ];
 
-// INSTALL
-self.addEventListener("install", event => {
+// Install Service Worker and Cache Assets
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
     })
   );
-  self.skipWaiting();
 });
 
-// ACTIVATE (clean old cache)
-self.addEventListener("activate", event => {
+// Activate and Clean up old caches
+self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      )
-    )
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
   );
-  self.clients.claim();
 });
 
-// FETCH (offline support)
-self.addEventListener("fetch", event => {
+// Fetch Assets from Cache
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
     })
   );
 });
